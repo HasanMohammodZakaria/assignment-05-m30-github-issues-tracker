9fa78134cc5b6c386463c9092b479433e279a014
Hasan Md Zakaria <zakariak4@gmail.com>
2026-03-09 20:14:12 +0600

    add search design and functionality

--- script/app.js
+++ script/app.js
@@ -57,7 +57,7 @@ const issuesFilter = (status) => {
     if (status === "all") {
         return allIssues;
     };
-    return allIssues.filter(issue => issue.status === status);
+    return allIssues.filter(issue => issue.status.toLowerCase() === status.toLowerCase());
 };
 
 const countsUpdate = (issues) => {
@@ -74,8 +74,8 @@ const priorityColors = {
 };
 
 const statusBorderColors = {
-    OPEN: 'border-t-4 border-[#00A96E]',
-    CLOSED: 'border-t-4 border-[#A855F7]'
+    open: 'border-t-4 border-[#00A96E]',
+    closed: 'border-t-4 border-[#A855F7]'
 };
 
 const displayIssue = (issues) => {
@@ -84,12 +84,17 @@ const displayIssue = (issues) => {
     cardContainer.innerHTML = "";
 
     issues.forEach((issue) => {
-        console.log(issue);
+        //console.log(issue);
+
         const card = document.createElement("div");
+        card.dataset.id = issue.id;
 
         const priorityLevel = issue.priority.toUpperCase();
         const priorityLevelColors = priorityColors[priorityLevel] || 'bg-[#FEECEC] text-[#EF4444]';
-        const statusColor = statusBorderColors[issue.status.toUpperCase()] || 'border-t-4 border-[#00A96E]';
+        const statusKey = issue.status.trim().toLowerCase();
+        const statusColor = statusBorderColors[statusKey] || 'border-t-4 border-[#00A96E]';
+
+        //console.log("Status:", issue.status, "StatusKey:", statusKey, "StatusColor:", statusColor);
 
         const statusImg = issue.status === 'open' ? './assets/Open-Status.png' : './assets/Closed-Status.png';
 
@@ -145,7 +150,156 @@ const displayIssue = (issues) => {
     });
 };
 
+
+const issuesContainer = document.getElementById('card-container');
+issuesContainer.addEventListener('click', (e) => {
+    const cardTracker = e.target.closest('.card-tracker');
+    if (!cardTracker) {
+        return;
+    };
+
+    const id = cardTracker.dataset.id;
+    //my_modal_5.showModal();
+    loadIssueDetail(id);
+});
+
+const loadIssueDetail = async (id) => {
+    const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
+
+    const res = await fetch(url);
+    const details = await res.json();
+    displayIssueDetail(details.data);
+
+}
+
+
+const statusBgColors = {
+    open: 'bg-[#00A96E] text-white',
+    close: 'bg-[#A855F7] text-white'
+};
+
+const displayIssueDetail = (issue) => {
+    console.log(issue);
+
+    // assignee
+    // : 
+    // "john_doe"
+    // author
+    // : 
+    // "security_sam"
+    // createdAt
+    // : 
+    // "2024-01-20T09:00:00Z"
+    // description
+    // : 
+    // "Implement JWT-based authentication with login, registration, and password reset functionality."
+    // id
+    // : 
+    // 5
+    // labels
+    // : 
+    // ['enhancement']
+    // priority
+    // : 
+    // "high"
+    // status
+    // : 
+    // "open"
+    // title
+    // : 
+    // "Add user authentication system"
+    // updatedAt
+    // : 
+    // "2024-01-20T09:00:00Z"
+
+    const bgStatusColor = statusBgColors[issue.status.toLowerCase()] || 'bg-[#00A96E] text-white';
+
+    const dateOnly = new Date(issue.createdAt).toLocaleDateString("en-US");
+
+    const priorityLevel = issue.priority.toUpperCase();
+    const priorityBgColors = priorityColors[priorityLevel] || 'bg-[#FEECEC] text-[#EF4444]';
+
+
+    const detailsBox = document.getElementById('details-container');
+    detailsBox.innerHTML = `
+                    <div class="">
+                        <h2 class="text-6 text-[#1F2937] font-bold">${issue.title}</h2>
+                    </div>
+                    <div class="flex items-center gap-2">
+                        <button class="bg-[#00A96E] text-white px-4 py-2 rounded-4xl ${bgStatusColor}">${issue.status}</button>
+                        <p class="text-[12px] text-[#64748B]">
+                            <i class="fa-solid fa-circle text-[8px]"></i>
+                            Opened by ${issue.assignee}
+                        </p>
+                        <p class="text-[12px] text-[#64748B]">
+                            <i class="fa-solid fa-circle text-[8px]"></i>
+                            ${dateOnly}
+                        </p>
+                    </div>
+                    <div class="level-container flex gap-2 flex-wrap">
+
+                        <button
+                            class="text-[#EF4444] text-[12px] font-medium bg-[#FEECEC] border border-[#FECACA] px-4 py-2 rounded-4xl">
+                            <i class="fa-solid fa-bug"></i>
+                            BUG
+                        </button>
+
+                        <button
+                            class="text-[#D97706] text-[12px] font-medium bg-[#FFF8DB] border border-[#FDE68A] px-4 py-2 rounded-4xl">
+                            <i class="fa-chisel fa-regular fa-life-ring"></i>
+                            HELP WANTED
+                        </button>
+                    </div>
+                    <p class="text-[12px] text-[#64748B]">
+                        ${issue.description}
+                    </p>
+                    <div class="bg-[#F8FAFC] p-4 rounded-lg flex ">
+                        <div class="w-1/2 flex flex-col">
+                            <p class="text-4 text-[#64748B]">Assignee:</p>
+                            <h4 class="text-4 text-[#1F2937] font-semibold">${issue.assignee}</h4>
+                        </div>
+                        <div class="w-1/2">
+                            <p class="text-4 text-[#64748B]">Priority:</p>
+                            <div class="w-20 h-6 badge text-3 p-2 rounded-4xl ${priorityBgColors}">
+                                ${priorityLevel}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-action">
+                    <form method="dialog">
+                        <!-- if there is a button in form, it will close the modal -->
+                        <button class="btn">Close</button>
+                    </form>
+                </div>
+
+
+    `;
+    document.getElementById('issue_modal').showModal();
+}
+
 loadIssue();
+
+const searchBtn = document.getElementById('btn-search');
+searchBtn.addEventListener('click', () => {
+    const searchInput = document.getElementById('input-search');
+    const searchValue = searchInput.value.trim().toLowerCase();
+    //console.log(searchValue);
+
+    // const searchUrl = 'https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q={searchText}';
+
+    manageSpinner(true);
+
+    fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${searchValue}`)
+        .then((res) => res.json())
+        .then((data) => {
+            displayIssue(data.data);
+            manageSpinner(false);
+        });
+
+});
+
+
 
 
 
